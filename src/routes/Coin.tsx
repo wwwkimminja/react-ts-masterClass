d7f9be55aaf4bfccb4ab86abd34518f36c20a4a4
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2024-07-02 20:09:23 +0900

    add components in Coin

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -87,6 +87,29 @@ interface PriceData {
   };
 } 
 
+
+const Overview = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background-color: rgba(0,0,0,0.5);
+  padding: 10px 20px;
+  border-radius: 10px;
+`
+const OverviewItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  span:first-child{
+    font-size:10px;
+    font-weight: 400;
+    text-transform: uppercase;
+    margin-bottom: 5px;
+  }
+`
+
+const Description = styled.p`
+  margin:20px 0px;
+`
 const Coin = () => {
   const [loading,setLoading]=useState(true);
   const [info,setInfo]= useState<InfoData>();
@@ -101,17 +124,47 @@ const Coin = () => {
      const priceData=await (await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)).json();
      setInfo(infoData);
      setPriceInfo(priceData);
-
+     setLoading(false);
     })();
-  },[])
+  },[coinId])
 
 
   return (
     <Container>
       <Header>
-        <Title>{state?.name||"Loading..."}</Title>
+        <Title>{state?.name?state.name:loading?"Loading...":info?.name}</Title>
       </Header>
-      {loading ? <Loader>Loading...</Loader>:<span></span>}
+      {loading ? <Loader>Loading...</Loader>:(
+        <>
+        <Overview>
+          <OverviewItem>
+            <span>Rank:</span>
+            <span>{info?.rank}</span>
+          </OverviewItem>
+          <OverviewItem>
+            <span>Symbol:</span>
+            <span>${info?.symbol}</span>
+          </OverviewItem>
+          <OverviewItem>
+            <span>Open Source:</span>
+            <span>{info?.open_source}</span>
+          </OverviewItem>
+          </Overview>
+          <Description>
+            {info?.description}
+          </Description>
+          <Overview>
+            <OverviewItem>
+            <span>Total Supply:</span>
+            <span>{priceInfo?.total_supply}</span>
+            </OverviewItem>
+            <OverviewItem>
+            <span>Max Supply:</span>
+            <span>{priceInfo?.max_supply}</span>
+            </OverviewItem>
+          </Overview>
+        </>
+      )}
     </Container>
 
   )
